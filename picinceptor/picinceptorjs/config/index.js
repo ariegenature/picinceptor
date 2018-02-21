35ea8dd6919d3f3f52d94599c7d06fcfef31dbad
Yg Versil <ygversil@openmailbox.org>
2018-02-21 15:12:21 +0100

    [js] Make JS build put files into Flask blueprint

--- picinceptor/picinceptorjs/config/index.js
+++ picinceptor/picinceptorjs/config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../views/contribute/templates/vue/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, '../../views/contribute/templates/static'),
+    assetsSubDirectory: 'vue',
+    assetsPublicPath: '/contribute/static',
 
     /**
      * Source Maps
