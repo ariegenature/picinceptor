52e4274fd429a63944ed304e5c6f676640562ca2
Yg Versil <ygversil@openmailbox.org>
2018-02-21 15:42:58 +0100

    [cli] Add commands to install JS client deps and build it

--- picinceptor/picinceptor_cli.py
+++ picinceptor/picinceptor_cli.py
@@ -5,7 +5,37 @@
 # then you can simply run `flask run`. See <http://flask.pocoo.org/docs/0.12/cli/> for more
 # information.
 
+import os
+import subprocess
+
+import click
+
 from picinceptor import create_app, read_config
+import picinceptor
+
 
 config = read_config()
 app = create_app(config)
+
+
+@app.cli.command(name='install-js-deps')
+def install_js_deps():
+    """Run ``npm install`` for the Vue.js client in order to install its JavaScript dependencies."""
+    click.echo('-> Installing JavaScript dependencies for the Vue.js client...')
+    subprocess.check_call(['npm',
+                           '--prefix={0}'.format(os.path.join(os.path.dirname(picinceptor.__file__),
+                                                              'picinceptorjs')),
+                           'install'])
+    click.echo('-> JavaScript dependencies succesfully installed.')
+
+
+@app.cli.command(name='build-js-client')
+def build_js_client():
+    """Execute ``npm run build`` for the Vue.js client to build it so that it can be served."""
+    click.echo('-> Building the Vue.js client...')
+    subprocess.check_call(['npm',
+                           '--prefix={0}'.format(os.path.join(os.path.dirname(picinceptor.__file__),
+                                                              'picinceptorjs')),
+                           'run',
+                           'build'])
+    click.echo('-> Vue.js client succesfully built.')
