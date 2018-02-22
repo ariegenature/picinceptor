1ecf2f6cb94684617868ea0c541c336e1ea044e4
Yg Versil <ygversil@openmailbox.org>
2018-02-22 12:14:54 +0100

    [js] insert ContributeMap into ContributePage

--- picinceptor/picinceptorjs/src/components/ContributePage.vue
+++ picinceptor/picinceptorjs/src/components/ContributePage.vue
@@ -1,14 +1,31 @@
 <template>
   <main class="hero is-fullheight">
-  <div class="hero-body">
-    <div class="container is-fluid">
+  <div id="hero-map" class="hero-body">
+    <div id="container-map" class="container is-fluid">
+      <div id="map">
+        <contribute-map></contribute-map>
+      </div>
     </div>
   </div>
   </main>
 </template>
 
 <script>
+import ContributeMap from './ContributeMap'
+
 export default {
-  name: 'ContributePage'
+  name: 'ContributePage',
+  components: {
+    ContributeMap
+  }
 }
 </script>
+
+<style>
+#hero-map, #container-map {
+  height: 100%;
+}
+#map {
+  height: 90%;
+}
+</style>
