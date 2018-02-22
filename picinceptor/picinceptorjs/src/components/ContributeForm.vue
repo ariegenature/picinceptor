530dacba1eb5427257b5b13c5576a0ed29ce633b
Yg Versil <ygversil@openmailbox.org>
2018-02-22 16:32:28 +0100

    [js] Add date picker input on first tab

--- picinceptor/picinceptorjs/src/components/ContributeForm.vue
+++ picinceptor/picinceptorjs/src/components/ContributeForm.vue
@@ -4,7 +4,15 @@
                  step-size="xs" next-button-text="Suivant" back-button-text="Retour"
                  finish-button-text="Terminer" @on-complete="submitForm">
       <tab-content title="Date">
-        <p>Choix de la date.</p>
+        <div class="columns is-mobile is-centered">
+          <div class="column is-narrow has-text-centered">
+            <b-field>
+              <b-datepicker inline size="is-small" v-model="observationDate"
+                            :month-names="monthNames" :day-names="dayNames"
+                            :first-day-of-week="1"></b-datepicker>
+            </b-field>
+          </div>
+        </div>
       </tab-content>
       <tab-content title="Espèce de pic">
         <p>Choix de l'espèce de pic.</p>
@@ -23,8 +31,19 @@
 </template>
 
 <script>
+const MONTH_NAMES = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août',
+  'Septembre', 'Octobre', 'Novembre', 'Décembre']
+const DAY_NAMES = ['dim.', 'lun.', 'mar.', 'mer.', 'jeu.', 'ven.', 'sam.']
+
 export default {
   name: 'ContributeForm',
+  data () {
+    return {
+      monthNames: MONTH_NAMES,
+      dayNames: DAY_NAMES,
+      observationDate: new Date()
+    }
+  },
   methods: {
     submitForm () {
       this.$emit('form-complete')
