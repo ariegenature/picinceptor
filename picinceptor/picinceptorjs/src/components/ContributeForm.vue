c0254132103c0fb835f33b2feaf26cf7658dfa76
Yg Versil <ygversil@openmailbox.org>
2018-02-22 17:40:34 +0100

    [js] Add select input on third tab about nesting index

--- picinceptor/picinceptorjs/src/components/ContributeForm.vue
+++ picinceptor/picinceptorjs/src/components/ContributeForm.vue
@@ -43,7 +43,20 @@
         </div>
       </tab-content>
       <tab-content title="Indice de nidification">
-        <p>Indice de nidification.</p>
+        <div class="columns is-centered">
+          <div class="column is-narrow">
+            <b-field>
+              <b-select placeholder="Sélectionnez un indice" icon="view-list" size="is-small"
+                        expanded v-model="nestingIndex">
+                <option v-for="index in nestingIndices"
+                        :value="index.id"
+                        :key="index.id">
+                    {{ index.name }}
+                </option>
+              </b-select>
+            </b-field>
+          </div>
+        </div>
       </tab-content>
       <tab-content title="Habitat observé">
         <p>Description de l'habitat observé.</p>
@@ -108,8 +121,75 @@ export default {
           img: 'https://openclipart.org/image/64px/svg_to_png/170814/yaffle.png'
         }
       ],
+      nestingIndices: [
+        {
+          id: '00',
+          name: 'Absence de réponse à la repasse'
+        },
+        {
+          id: '01',
+          name: 'Oiseau retrouvé mort, écrasé'
+        },
+        {
+          id: '02',
+          name: 'Oiseau vu en période de nidification (février à juillet) dans un milieu favorable'
+        },
+        {
+          id: '03',
+          name: 'Mâle chanteur (et/ou cris nuptiaux et/ou tambourinage) présent en période de nidification dans un milieu favorable'
+        },
+        {
+          id: '04',
+          name: 'Couple présent en période de reproduction dans un milieu favorable'
+        },
+        {
+          id: '05',
+          name: 'Comportement territorial (chant, querelles avec des voisins, etc.) observé sur un même site à au moins une semaine d\'intervalle. Observation simultanée de deux mâles chanteurs ou plus sur un même site'
+        },
+        {
+          id: '06',
+          name: 'Parades, accouplement ou échange de nourriture entre adultes'
+        },
+        {
+          id: '07',
+          name: 'Cri d\'alarme ou tout autre comportement agité indiquant la proximité d\'un nid ou de jeunes'
+        },
+        {
+          id: '09',
+          name: 'Transport de matériel ou construction d\'un nid ; forage d\'une cavité'
+        },
+        {
+          id: '11',
+          name: 'Nid vide ayant été utilisé ou contenant des coquilles d’œufs'
+        },
+        {
+          id: '12',
+          name: 'Jeunes en duvet ou jeunes venant de quitter le nid et incapables de soutenir le vol sur de longues distances'
+        },
+        {
+          id: '13',
+          name: 'Adulte gagnant, occupant ou quittant le site d\'un nid ; comportement révélateur d\'un nid occupé dont le contenu ne peut être vérifié (trop haut ou dans une cavité)'
+        },
+        {
+          id: '14',
+          name: 'Adulte transportant de la nourriture pour les jeunes'
+        },
+        {
+          id: '15',
+          name: 'Adulte couvant'
+        },
+        {
+          id: '16',
+          name: 'Nid avec des œufs ou des jeunes (vus ou entendus)'
+        },
+        {
+          id: '17',
+          name: 'Oiseau vu hors période de nidification, août à janvier'
+        }
+      ],
       observationDate: new Date(),
-      woodpeckerId: null
+      woodpeckerId: null,
+      nestingIndex: null
     }
   },
   methods: {
