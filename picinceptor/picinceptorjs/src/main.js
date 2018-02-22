56843759b994d5fcde8a99e73004eb8f8363e1b1
Yg Versil <ygversil@openmailbox.org>
2018-02-22 11:25:03 +0100

    [js] Add vue2-leaflet as a dependency
    And use it in main.js along with Leaflet CSS.

--- picinceptor/picinceptorjs/src/main.js
+++ picinceptor/picinceptorjs/src/main.js
@@ -3,12 +3,16 @@
 import App from './App'
 import Buefy from 'buefy'
 import Vue from 'vue'
+import Vue2Leaflet from 'vue2-leaflet'
 import router from './router'
 import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
+
+Vue.component('v-map', Vue2Leaflet.Map)
+Vue.component('v-tilelayer', Vue2Leaflet.TileLayer)
 
 /* eslint-disable no-new */
 new Vue({
