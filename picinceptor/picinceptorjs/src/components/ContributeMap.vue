cbee97761c168bbada99f6b2d9a980c19ab6616d
Yg Versil <ygversil@openmailbox.org>
2018-02-22 14:05:31 +0100

    [js] Insert LeafletDraw into ContributeMap
    Only allow markers for this particular woodpeckers application.

--- picinceptor/picinceptorjs/src/components/ContributeMap.vue
+++ picinceptor/picinceptorjs/src/components/ContributeMap.vue
@@ -2,12 +2,19 @@
   <v-map ref="map" :zoom="zoom" :center="center">
     <v-tilelayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                  attribution="OpenStreetMap contributors"></v-tilelayer>
+    <leaflet-draw :marker="true" :polyline="false" :polygon="false" :rectangle="false"
+                  :circle="false" :circle-marker="false"></leaflet-draw>
   </v-map>
 </template>
 
 <script>
+import LeafletDraw from './LeafletDraw'
+
 export default {
   name: 'ContributeMap',
+  components: {
+    LeafletDraw
+  },
   data () {
     return {
       center: [42.857846, 0.626220],
