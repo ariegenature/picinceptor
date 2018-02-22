f2eae48aca4d3abadc6ab3905e2d9ccaa47f01f2
Yg Versil <ygversil@openmailbox.org>
2018-02-22 10:31:09 +0100

    [js] Add buefy as a dependency
    After running `npm --prefix=picinceptor/picinceptorjs/ install --save buefy`.
    
    Also use it in `main.js` along with its CSS.

--- picinceptor/picinceptorjs/src/main.js
+++ picinceptor/picinceptorjs/src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import App from './App'
+import Buefy from 'buefy'
 import Vue from 'vue'
 import router from './router'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
