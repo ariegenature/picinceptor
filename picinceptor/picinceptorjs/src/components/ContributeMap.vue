2ab13a0ec113497a7843bf638179c8a16ccdd02d
Yg Versil <ygversil@openmailbox.org>
2018-02-22 12:13:35 +0100

    [js] Add first version of ContributeMap component
    With hardcoded values for initial center and zoom.

---
+++ picinceptor/picinceptorjs/src/components/ContributeMap.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-map ref="map" :zoom="zoom" :center="center">
+    <v-tilelayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                 attribution="OpenStreetMap contributors"></v-tilelayer>
+  </v-map>
+</template>
+
+<script>
+export default {
+  name: 'ContributeMap',
+  data () {
+    return {
+      center: [42.857846, 0.626220],
+      zoom: 8
+    }
+  }
+}
+</script>
