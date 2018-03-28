b5013e5a8f751eaf7ed330962395bad76a545a05
Yg Versil <ygversil@openmailbox.org>
2018-03-28 11:49:06 +0200

    [api] Rename parser -> post_parser
    Since there will be a `get_parser` soon.

--- picinceptor/views/api/__init__.py
+++ picinceptor/views/api/__init__.py
@@ -20,37 +20,39 @@ class ObservationResource(Resource):
     """Flask-Restful API endpoint around observations."""
 
     def __init__(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('observationDate', dest='observation_date',
-                                 type=_parse_json_date, required=True, nullable=False,
-                                 help='Observation date')
-        self.parser.add_argument('woodpeckerId', dest='woodpecker', type=int, required=True,
-                                 nullable=False, help='TAXREF id of the woodpecker')
-        self.parser.add_argument('nestingIndex', dest='breeding_code', type=int, required=True,
-                                 nullable=False, help='Woodpecker breeding code')
-        self.parser.add_argument('habitat', type=str, required=True, nullable=False,
-                                 help='Habitat where woodpecker has been seen')
-        self.parser.add_argument('dominant', dest='dominant_tree', type=str, required=False,
-                                 help='Dominant tree in the forest where woodpecker has been seen')
-        self.parser.add_argument('hasDeadTrees', dest='has_dead_trees', type=bool, required=False,
-                                 help=('Are there any dead trees in the forest where woodpecker '
-                                       'has been seen'))
-        self.parser.add_argument('hasConifer', dest='has_conifers', type=bool, required=False,
-                                 help=('Are there any conifers in the forest where woodpecker '
-                                       'has been seen'))
-        self.parser.add_argument('firstName', dest='observer_first_name', type=str, required=True,
-                                 nullable=False, help='Observer first name')
-        self.parser.add_argument('surname', dest='observer_surname', type=str, required=True,
-                                 nullable=False, help='Observer surname')
-        self.parser.add_argument('email', dest='observer_email', type=str, required=True,
-                                 nullable=False, help='Observer email')
-        self.parser.add_argument('school', dest='observer_school', type=str, default='',
-                                 nullable=False, help='Observer school')
-        self.parser.add_argument('eWkt', dest='geometry', type=str,
-                                 required=True, nullable=False, help='Observation coordinates')
+        self.post_parser = reqparse.RequestParser()
+        self.post_parser.add_argument('observationDate', dest='observation_date',
+                                      type=_parse_json_date, required=True, nullable=False,
+                                      help='Observation date')
+        self.post_parser.add_argument('woodpeckerId', dest='woodpecker', type=int, required=True,
+                                      nullable=False, help='TAXREF id of the woodpecker')
+        self.post_parser.add_argument('nestingIndex', dest='breeding_code', type=int, required=True,
+                                      nullable=False, help='Woodpecker breeding code')
+        self.post_parser.add_argument('habitat', type=str, required=True, nullable=False,
+                                      help='Habitat where woodpecker has been seen')
+        self.post_parser.add_argument('dominant', dest='dominant_tree', type=str, required=False,
+                                      help=('Dominant tree in the forest where woodpecker has been '
+                                            'seen'))
+        self.post_parser.add_argument('hasDeadTrees', dest='has_dead_trees', type=bool,
+                                      required=False, help=('Are there any dead trees in the '
+                                                            'forest where woodpecker has been '
+                                                            'seen'))
+        self.post_parser.add_argument('hasConifer', dest='has_conifers', type=bool, required=False,
+                                      help=('Are there any conifers in the forest where woodpecker '
+                                            'has been seen'))
+        self.post_parser.add_argument('firstName', dest='observer_first_name', type=str,
+                                      required=True, nullable=False, help='Observer first name')
+        self.post_parser.add_argument('surname', dest='observer_surname', type=str, required=True,
+                                      nullable=False, help='Observer surname')
+        self.post_parser.add_argument('email', dest='observer_email', type=str, required=True,
+                                      nullable=False, help='Observer email')
+        self.post_parser.add_argument('school', dest='observer_school', type=str, default='',
+                                      nullable=False, help='Observer school')
+        self.post_parser.add_argument('eWkt', dest='geometry', type=str,
+                                      required=True, nullable=False, help='Observation coordinates')
 
     def post(self):
-        observation_dict = self.parser.parse_args(strict=True)
+        observation_dict = self.post_parser.parse_args(strict=True)
         insert_query = anosql.load_queries(
             'postgres',
             os.path.join(os.path.dirname(picinceptor.__file__), 'database',
