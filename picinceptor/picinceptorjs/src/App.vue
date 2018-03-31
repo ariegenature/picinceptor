a7e12a033fb43a5b92629b4a5e55ca8394477436
Yg Versil <ygversil@openmailbox.org>
2018-03-31 13:22:33 +0200

    [js] Add footer

--- picinceptor/picinceptorjs/src/App.vue
+++ picinceptor/picinceptorjs/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
     <router-view/>
+      <site-footer></site-footer>
   </div>
 </template>
 
 <script>
+import SiteFooter from './components/SiteFooter'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    SiteFooter
+  }
 }
 </script>
---
+++ picinceptor/picinceptorjs/src/components/SiteFooter.vue
@@ -0,0 +1,84 @@
+<template>
+  <footer class="footer is-size-7 has-text-centered has-text-weight-light has-text-grey-darker">
+    <div class="container is-fluid">
+      <div class="columns">
+        <div class="column is-one-third vcenter">
+          <div class="content">
+            <div>
+              <div class="image" style="display: inline-block;">
+                <a href="http://flask.pocoo.org/" target="_blank">
+                  <img src="static/logo-flask.png"
+                       border="0" alt="Logo Flask" title="Propulsé par Flask"
+                       style="height: 16px; width: auto;">
+                </a>
+              </div>&nbsp;
+              <div class="image is-16x16" style="display: inline-block;">
+                <a href="https://vuejs.org" target="_blank">
+                  <img src="static/logo-vuejs.png"
+                       boroder="0" alt="Logo Vue.js" title="Propulsé par Vue.js">
+                </a>
+              </div>
+            </div>
+            <p>Réalisé avec
+            <a href="http://flask.pocoo.org" target="_blank" title="Flask">Flask</a>
+            et
+            <a href="https://vuejs.org" target="_blank" title="Vue.js">Vue.js</a>.</p>
+            <p>Copyright © ANA 2018.</p>
+          </div>
+        </div>
+        <div class="column is-one-third vcenter">
+          <div class="content">
+            <div class="block">
+              <div class="image" style="display: inline-block;">
+                <a href="https://www.poctefa.eu/fr/" target="_blank">
+                  <img src="static/logo-poctefa.jpg"
+                       border="0" alt="Logo POCTEFA" title="POCTEFA"
+                       style="height: 64px; width: auto;">
+                </a>
+              </div>
+            </div>
+            <p>Enquête participative dans le cadre du projet
+            <a href="https://ariegenature.fr/activites/enquete-participative-sur-les-pics-pyreneens/habios/"
+               target="_blank" title="Projet HABIOS">
+              HABIOS
+            </a>. Le projet HABIOS a été cofinancé à hauteur de 65% par le Fonds
+            Européen de Développement Régional (FEDER) au travers du
+            <a href="https://www.poctefa.eu/fr/" target="_blank" title="POCTEFA">
+            Programme Interreg V-A Espagne-France-Andorre
+            </a>(POCTEFA 2014-2020).</p>
+            <p>Ce projet est relayé par le réseau transfrontalier Education
+            Pyrénées Vivantes.</p>
+          </div>
+        </div>
+        <div class="column is-one-third vcenter">
+          <div class="block">
+            <a href="https://www.facebook.com/ariegenature/" target="_blank" title="Facebook ANA">
+              <b-icon icon="facebook" type="is-dark"></b-icon>
+            </a>
+            &emsp;
+            <a href="https://www.youtube.com/channel/UCjIAs3vAZxZT33x3eBI5t4w" target="_blank" title="Chaîne Youtube ANA">
+              <b-icon icon="youtube-play" type="is-dark"></b-icon>
+            </a>
+            &emsp;
+            <a href="https://ariegenature.fr" target="_blank" title="Facebook ANA">
+              <b-icon icon="web" type="is-dark"></b-icon>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'SiteFooter'
+}
+</script>
+
+<style>
+.vcenter {
+  margin-top: auto;
+  margin-bottom: auto;
+}
+</style>
